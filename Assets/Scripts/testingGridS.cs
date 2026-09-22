463659bb70d016b0edbed2a26c952676e75af570
RetsunoThe <ryanrdm19@gmail.com>
2026-09-22 01:16:08 -0300

    omg finally I can push git in peace

--- Assets/Scripts/testingGridS.cs
+++ Assets/Scripts/testingGridS.cs
@@ -3,7 +3,7 @@ using UnityEngine;
 
 public class testingGridS : MonoBehaviour
 {
-
+    int finall = 1;
     Vector3 animatronicPosition;
     Vector3 destination;
 
@@ -11,6 +11,8 @@ public class testingGridS : MonoBehaviour
     void Start()
     {
         GridF gridPathfind = new GridF(10, 5, 2f);
+
+        //Omg finally I can  git in peace
 
 
     }
